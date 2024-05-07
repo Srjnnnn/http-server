8ae35cf80907f69a80b27b387582c7181c856820
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2024-05-07 23:04:16 +0300

    Add custom error, parse method and protocol

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,5 +1,11 @@
-use super::method::Method;
+use crate::http::{method, request};
+
+use super::method::{Method, MethodError};
 use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Debug, Result as FmtResult};
+use std::fmt::{Display, Formatter};
+use std::str::{self, Utf8Error};
 pub struct Request {
     path: String,
     query_string: Option<String>,
@@ -7,9 +13,80 @@ pub struct Request {
 }
 
 impl TryFrom<&[u8]> for Request {
-    type Error = String;
+    type Error = ParseError;
 
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        let request = str::from_utf8(buf)?;
+
+        // match parse_sentence(request) {
+        //     Some((method, request)) => {}
+        //     None => return Err(ParseError::InvalidRequest),
+        // }
+
+        let (method, request) = parse_sentence((request)).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = parse_sentence((request)).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = parse_sentence((request)).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+
+        let method: Method = method.parse()?;
+
         unimplemented!()
     }
 }
+
+fn parse_sentence(request: &str) -> Option<(&str, &str)> {
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' || c == '\r' {
+            return Some((&request[..i], &request[i + 1..]));
+        }
+    }
+
+    None
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidMethod => "Invalid Method",
+        }
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
+    }
+}
+
+impl From<MethodError> for ParseError {
+    fn from(_: MethodError) -> Self {
+        Self::InvalidMethod
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {}
