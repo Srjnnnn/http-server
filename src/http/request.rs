d36c7e11eaf0836c57425ce9a59d92877c5dd8b2
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2024-05-07 23:16:25 +0300

    Implement the query string

--- src/http/request.rs
+++ src/http/request.rs
@@ -24,7 +24,7 @@ impl TryFrom<&[u8]> for Request {
         // }
 
         let (method, request) = parse_sentence((request)).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = parse_sentence((request)).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = parse_sentence((request)).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = parse_sentence((request)).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1" {
@@ -32,6 +32,12 @@ impl TryFrom<&[u8]> for Request {
         }
 
         let method: Method = method.parse()?;
+
+        let mut query_string = None;
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i + 1..]);
+            path = &path[..i];
+        }
 
         unimplemented!()
     }
